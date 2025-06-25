1631713501dc22226ab5ec2ad4418f32ef22de6e
Avgustina Shevchuk <avgustina.shevchuk@lemon.markets>
2025-06-25 22:54:28 +0200

    connect to a postgres datasource and add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.postgresql:postgresql:42.7.3")
 //	implementation("org.liquibase:liquibase-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
