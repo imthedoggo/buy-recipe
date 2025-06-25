ba2cb4f12577abd4fe349b1cf5dafe6272a9dbe8
Avgustina Shevchuk <avgustina.shevchuk@lemon.markets>
2025-06-25 22:39:04 +0200

    refactor recipe repository

--- src/main/kotlin/de/shevchuk/buy_recipe/model/RecipeRepository.kt
+++ src/main/kotlin/de/shevchuk/buy_recipe/model/RecipeRepository.kt
@@ -4,8 +4,6 @@ import de.shevchuk.buy_recipe.dto.*
 import de.shevchuk.buy_recipe.entitiy.*
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
--- src/test/kotlin/de/shevchuk/buy_recipe/unitests/RecipeServiceTest.kt
+++ src/test/kotlin/de/shevchuk/buy_recipe/unitests/RecipeServiceTest.kt
@@ -8,7 +8,10 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.*
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.whenever
 
 class RecipeServiceTest {
     private lateinit var recipeRepository: RecipeRepository
