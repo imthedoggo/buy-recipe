9e7e7963616246aa724ea722a88d4691b8422b79
Avgustina Shevchuk <avgustina.shevchuk@lemon.markets>
2025-06-25 23:19:18 +0200

    enable liquibase and init tables

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.postgresql:postgresql:42.7.3")
-//	implementation("org.liquibase:liquibase-core")
+	implementation("org.liquibase:liquibase-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
